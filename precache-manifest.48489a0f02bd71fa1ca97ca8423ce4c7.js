50cc098029b4c0eb7b21f4a4213ea613c6f0d126
Hamza Ashfaq <hamzaashfaq7866@gmail.com>
2020-09-19 23:17:58 -0700

    Updates

---
+++ precache-manifest.48489a0f02bd71fa1ca97ca8423ce4c7.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6d26fe95f72c0c7122d9d66ff680f6a0",
+    "url": "/haze_theme/index.html"
+  },
+  {
+    "revision": "d0267b46a784f735f6ea",
+    "url": "/haze_theme/static/css/main.2f1b4222.chunk.css"
+  },
+  {
+    "revision": "0f97573567613fce6924",
+    "url": "/haze_theme/static/js/2.1d42a855.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/haze_theme/static/js/2.1d42a855.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0267b46a784f735f6ea",
+    "url": "/haze_theme/static/js/main.29e37409.chunk.js"
+  },
+  {
+    "revision": "15fead2e8d7560bbecfe",
+    "url": "/haze_theme/static/js/runtime-main.d31499f8.js"
+  },
+  {
+    "revision": "f53f90b10c50c8caf684003efd70833f",
+    "url": "/haze_theme/static/media/about-us.f53f90b1.jpg"
+  },
+  {
+    "revision": "6d009055d0293548fe79037e2732f118",
+    "url": "/haze_theme/static/media/hero-bg.6d009055.jpg"
+  },
+  {
+    "revision": "d878b6359086a0b846613e7f5a25c7f5",
+    "url": "/haze_theme/static/media/member-1.d878b635.jpg"
+  },
+  {
+    "revision": "a808b7c863811ddf9f65d7c5c5873cc3",
+    "url": "/haze_theme/static/media/member-2.a808b7c8.jpg"
+  },
+  {
+    "revision": "79020031f63c52e5652936ace9a54c1c",
+    "url": "/haze_theme/static/media/member-3.79020031.jpg"
+  },
+  {
+    "revision": "145078dd718027b92aec794077f2ec55",
+    "url": "/haze_theme/static/media/portfolio-1.145078dd.jpg"
+  },
+  {
+    "revision": "cd1042b98e839ea36dd2902e3c77173c",
+    "url": "/haze_theme/static/media/portfolio-2.cd1042b9.jpg"
+  },
+  {
+    "revision": "bb9ca2d88f58cc1712aca52535087321",
+    "url": "/haze_theme/static/media/portfolio-3.bb9ca2d8.jpg"
+  },
+  {
+    "revision": "9b29b09257b6a0154a2b7058d0753d81",
+    "url": "/haze_theme/static/media/portfolio-4.9b29b092.jpg"
+  }
+]);
